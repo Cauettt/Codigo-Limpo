e2d760c60b3dbee85f3d468e156f93c63e361764
Cauettt <cauetrodrigues@gmail.com>
2025-05-05 15:08:35 -0300

    Update 01-easy Single-Responsibility.js

--- single_responsability/01-easy Single-Responsibility.js
+++ single_responsability/01-easy Single-Responsibility.js
@@ -1,15 +1,11 @@
-/**
- * Exercício 1:
- * Refatore a função abaixo em múltiplas funções, cada uma com responsabilidade única.
- */
 
-function processarDadosUsuario(usuario) {
-  // Validação
+function validarDadosUsuario(usuario) {
   if (!usuario.nome || !usuario.email) {
     return { sucesso: false, mensagem: "Nome e email são obrigatórios" };
   }
+}
 
-  // Formatação
+function formatarNomeUsuario(usuario) {
   const nomeFormatado = usuario.nome
     .trim()
     .toLowerCase()
@@ -17,13 +13,23 @@ function processarDadosUsuario(usuario) {
     .map((parte) => parte.charAt(0).toUpperCase() + parte.slice(1))
     .join(" ");
 
+   return nomeFormatado
+}
+
+function formatarEmailUsuario(usuario) {
+
   const emailFormatado = usuario.email.trim().toLowerCase();
 
-  // Persistência
-  console.log(`Salvando usuário: ${nomeFormatado}, ${emailFormatado}`);
+  return emailFormatado
+}
+
+
+function salvarDadosUsuario(usuario) {
+
+  console.log(`Salvando usuário: ${formatarNomeUsuario()}, ${formatarEmailUsuario()}`);
 
   return {
     sucesso: true,
-    usuario: { nome: nomeFormatado, email: emailFormatado },
+    usuario: { nome: formatarNomeUsuario(), email: formatarEmailUsuario() },
   };
 }
