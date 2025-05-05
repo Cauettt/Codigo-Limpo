3030a2cd273c9e23f3172ff074ee0f941e45db26
Cauettt <cauetrodrigues@gmail.com>
2025-05-05 14:47:03 -0300

    Update 03-medium Magic Numbers.js

--- magic_numbers/03-medium Magic Numbers.js
+++ magic_numbers/03-medium Magic Numbers.js
@@ -1,18 +1,21 @@
-/**
- * Exercício 3: 
- * Refatore o algoritmo de cálculo de preço, 
- * substituindo todos os números mágicos por constantes significativas.
- */
 
 function calcularPrecoFinal(preco, quantidade) {
   let desconto = 0;
+  const descontoBaixo = 0.05
+  const descontoMedio = 0.1
+  const descontoAlto = 0.15
 
-  if (quantidade >= 10) {
-    desconto = 0.15;
-  } else if (quantidade >= 5) {
-    desconto = 0.1;
-  } else if (quantidade >= 3) {
-    desconto = 0.05;
+  const quantidadeMinima = 3
+  const quantidadeMedia = 5
+  const quantidadeAlta = 10
+
+
+  if (quantidade >= quantidadeAlta) {
+    desconto = descontoAlto;
+  } else if (quantidade >= quantidadeMedia) {
+    desconto = descontoMedio;
+  } else if (quantidade >= quantidadeMinima) {
+    desconto = descontoBaixo;
   }
   const multiplicadorPreco = 1
   const precoComDesconto = preco * (multiplicadorPreco - desconto);
